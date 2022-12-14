4ae549aafa706d19f41d14039cd74e3c8e4efa65
Rikard Olsson <rikard@thryselius.se>
2022-12-14 10:01:56 +0100

    groups result of benchmark for to_polyhedron

--- benches/puan_benchmark.rs
+++ benches/puan_benchmark.rs
@@ -3,13 +3,13 @@ use puanrs::{Theory};
 
 fn criterion_benchmark(c: &mut Criterion) {
     
-    let mut group = c.benchmark_group("to_polyhedron_w2_red");
+    let mut group = c.benchmark_group("to_polyhedron_w2");
     let static_width = 2;
-    for depth in 1..10 {
+    for depth in 1..16 {
         let theory = Theory::instance(static_width, depth, String::from("some-id"));
         group.throughput(Throughput::Bytes(depth as u64));
         group.bench_with_input(BenchmarkId::from_parameter(depth), &depth, |b, &depth| {
-            b.iter(|| theory.to_ge_polyhedron(true, true));
+            b.iter(|| theory.to_ge_polyhedron(true, false));
         });
     }
     group.finish();
